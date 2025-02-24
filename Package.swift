4af744d9ce893b14acfc1e02d059f9f84d6c93e7
Mark Granoff <mark@granoff.net>
2025-02-24 17:56:35 -0500

    Remove obsolete Obj-C implementation. Bump Pod version.

--- Package.swift
+++ Package.swift
@@ -18,10 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FFBadgedBarButtonItem"),
-        .testTarget(
-            name: "FFBadgedBarButtonItemTests",
-            dependencies: ["FFBadgedBarButtonItem"]
-        ),
+            name: "FFBadgedBarButtonItem"
+        )
     ]
 )
